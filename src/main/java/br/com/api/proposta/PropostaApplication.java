2b4601cee6dcf4ed7b39b72178a3082244f85328
Elias-Lima906 <eliaslima900@hotmail.com>
2021-03-25 14:38:42 -0300

    initial commit

---
+++ src/main/java/br/com/api/proposta/PropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
