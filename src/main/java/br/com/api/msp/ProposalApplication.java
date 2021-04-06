d2432a6c5cad5b3fbbbe7a18811cf16f8f87ef9d
Elias-Lima906 <eliaslima900@hotmail.com>
2021-04-06 16:16:16 -0300

    change package's name

---
+++ src/main/java/br/com/api/msp/ProposalApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.msp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalApplication.class, args);
+	}
+
+}
