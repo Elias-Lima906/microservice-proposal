6c21f91f62b559190fe1b4aa192805a245be3ec3
Elias-Lima906 <eliaslima900@hotmail.com>
2021-04-06 16:23:40 -0300

    create an address object to receive all address data

---
+++ src/main/java/br/com/api/msp/address/Address.java
@@ -0,0 +1,66 @@
+package br.com.api.msp.address;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.Nullable;
+
+@Embeddable
+public class Address {
+
+	@NotBlank
+	private String city;
+
+	@NotBlank
+	private String neighborhood;
+
+	@NotBlank
+	private String address;
+
+	@NotBlank
+	@Size(max = 9)
+	private String zipCode;
+
+	@Nullable
+	private String complement;
+
+	@Deprecated
+	public Address() {
+	}
+
+	public Address(@NotBlank String city, @NotBlank String neighborhood, @NotBlank String address,
+			@NotBlank @Size(max = 9) String zipCode, String complement) {
+		super();
+		this.city = city;
+		this.neighborhood = neighborhood;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.complement = complement;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+	
+	public AddressResponse toAddressDto() {
+		return new AddressResponse(this);
+	}
+
+}
